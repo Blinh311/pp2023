383142c7448ef4d70da2a0d83272e0a73ebdb06d
Blinh311 <banglinh.vu2003@gmail.com>
2023-02-24 11:13:35 +0700

    Update file

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,76 +1,92 @@
-# Initialize empty lists and dictionaries
-students = []
+import string
+
 courses = []
-marks = {}
+students = []
+marks = []
 
-# Function to input student information
-def input_student():
-    id = input("Enter student ID: ")
-    name = input("Enter student name: ")
-    dob = input("Enter student date of birth: ")
-    students.append((id, name, dob))
+# Input number of students in a class
+numOfStudents = int(input("How many students are there in a class ? \n"))
 
-# Function to input course information
-def input_course():
-    id = input("Enter course ID: ")
-    name = input("Enter course name: ")
-    courses.append((id, name))
+# Input student information: id, name, DoB
+for i in range(0, numOfStudents):
+    print(f"Student {i+1}'s informations: ")
+    id = int(input("Id: "))
+    name = input("Name: ")
+    dob = input("Dob (format: dd/mm/yy): ")
+    students.append({"id": id, "name": name, "Dob": dob})
 
-# Function to input marks for a given course and student
-def input_marks():
-    course_id = input("Enter course ID: ")
-    student_id = input("Enter student ID: ")
-    mark = float(input("Enter marks for {}: ".format(courses[courses.index((course_id,))][1])))
-    marks.setdefault(course_id, {})[student_id] = mark
+    i += 1
 
-# Function to list courses
-def list_courses():
-    print("Courses:")
-    for course in courses:
-        print("- {}: {}".format(course[0], course[1]))
+# Input number of courses
+numOfCourses = int(input("How many courses are there ? \n"))
 
-# Function to list students
-def list_students():
-    print("Students:")
-    for student in students:
-        print("- {}: {}".format(student[0], student[1]))
+# Input course information: id, name
+for j in range(0, numOfCourses):
+    print(f"Course {j+1}'s informations: ")
+    _id = int(input("Id: "))
+    _name = input("Name: ")
+    courses.append({"id": _id, "name": _name, "studentsList": students})
 
-# Function to show student marks for a given course
-def show_marks():
-    course_id = input("Enter course ID: ")
-    if course_id not in marks:
-        print("No marks found for course")
-        return
-    print("Marks for {}:".format(courses[courses.index((course_id,))][1]))
-    for student_id, mark in marks[course_id].items():
-        student_name = students[students.index((student_id,))][1]
-        print("- {}: {}".format(student_name, mark))
+    j += 1
 
-# Main program
-while True:
-    print("""
-    1. Input student information
-    2. Input course information
-    3. Input marks for a student in a course
-    4. List courses
-    5. List students
-    6. Show student marks for a course
-    7. Exit
-    """)
-    choice = input("Enter choice: ")
-    if choice == "1":
-        input_student()
-    elif choice == "2":
-        input_course()
-    elif choice == "3":
-        input_marks()
-    elif choice == "4":
-        list_courses()
-    elif choice == "5":
-        list_students()
-    elif choice == "6":
-        show_marks()
-    elif choice == "7":
+# Select a course, input marks for student in this course
+courseSelection = 1
+while courseSelection in range(1, numOfCourses + 1):
+    courseSelection = int(
+        input(
+            f"To enter marks for student, select a course (from 1 to {numOfCourses}, type 0 to quit): "
+        )
+    )
+
+    if courseSelection == 0:
         break
+
+    for k in range(0, numOfStudents):
+        print(f"Mark for student {students[k]['name']}: ")
+        mark = int(input())
+        marks.append({students[k]["name"]: mark})
+
+        k += 1
+
+    courses[courseSelection - 1]["marks"] = marks[
+        (courseSelection - 1) * numOfStudents : courseSelection * numOfStudents
+    ]
+
+choices = 1
+
+# Listing functions
+while choices < 4:
+    choices = int(
+        input(
+            "If you want to see courses list, please type 1, students list, type 2, student marks, type 3, another number to quit: "
+        )
+    )
+
+    # List courses
+    if choices == 1:
+        print("Id".ljust(5, " "), "Name")
+        for j in range(0, numOfCourses):
+            print(str(courses[j]["id"]).ljust(5, " "), courses[j]["name"])
+    # List students
+    elif choices == 2:
+        print("Id".ljust(5, " "), "Name".ljust(15, " "), "Dob")
+        for i in range(0, numOfStudents):
+            print(
+                str(students[i]["id"]).ljust(5, " "),
+                students[i]["name"].ljust(15, " "),
+                students[i]["Dob"],
+            )
+    # Show student marks for a given course
+    elif choices == 3:
+        for j in range(0, numOfCourses):
+            print(courses[j]["name"])
+            print("\tId".ljust(5, " "), "Name".ljust(15, " "), "Mark")
+            for i in range(0, numOfStudents):
+                print(
+                    "\t",
+                    str(students[i]["id"]).ljust(5, " "),
+                    students[i]["name"].ljust(15, " "),
+                    courses[j]["marks"][i][students[i]["name"]],
+                )
     else:
-        print("Invalid choice")
+        break
